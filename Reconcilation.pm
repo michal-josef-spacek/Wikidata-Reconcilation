d27ab964859f523d0c2a26335a130d4d0850b774
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-04-16 19:30:25 +0200

    First stub

---
+++ Reconcilation.pm
@@ -0,0 +1,90 @@
+package Wikidata::Reconcilation;
+
+use strict;
+use warnings;
+
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+use LWP::UserAgent;
+use WQS::SPARQL;
+use WQS::SPARQL::Result;
+
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;
+
+	# User agent.
+	$self->{'agent'} = __PACKAGE__." ($VERSION)";
+
+	# First match mode.
+	$self->{'first_match'} = 0;
+
+	# Language.
+	$self->{'language'} = 'en';
+
+	# LWP::UserAgent object.
+	$self->{'lwp_user_agent'} = undef;
+
+	# Verbose mode.
+	$self->{'verbose'} = 0;
+
+	# Process parameters.
+	set_params($self, @params);
+
+	if (! defined $self->{'lwp_user_agent'}) {
+		$self->{'lwp_user_agent'} = LWP::UserAgent->new(
+			'agent' => $self->{'agent'},
+		);
+	} else {
+		if (! $self->{'lwp_user_agent'}->isa('LWP::UserAgent')) {
+			err "Parameter 'lwp_user_agent' must be a 'LWP::UserAgent' instance.";
+		}
+	}
+
+	$self->{'_q'} = WQS::SPARQL->new(
+		'lwp_user_agent' => $self->{'lwp_user_agent'},
+	);
+
+	return $self;
+}
+
+sub reconcile {
+	my ($self, $reconcilation_rules_hr) = @_;
+
+	my @sparql = $self->_reconcile($reconcilation_rules_hr);
+
+	my $ret_hr;
+	my %qids;
+	foreach my $sparql (@sparql) {
+		if ($self->{'verbose'}) {
+			print "SPARQL query:\n";
+			print $sparql."\n";
+		}
+
+		$ret_hr = $self->{'_q'}->query($sparql);
+		my @ret = map { $_->{'item'} } WQS::SPARQL::Result->new->result($ret_hr);
+		foreach my $ret (@ret) {
+			$qids{$ret}++;
+		}
+		if (@ret && $self->{'first_match'}) {
+			last;
+		}
+	}
+
+	return sort keys %qids;
+}
+
+sub _reconcile {
+	my ($self, @params) = @_;
+
+	err "This is abstract class. You need to implement _reconcile() method.";
+}
+
+1;
+
+__END__
